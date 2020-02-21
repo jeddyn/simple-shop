49380840d2a32a140bf131bfda8e5e1d82a2d98b
MichalCZ4D <m11czarnacki@gmail.com>
2020-02-21 17:37:27 +0100

    renamed class and fields

--- src/service/ShoppingBasketService.java
+++ src/service/ShoppingBasketService.java
@@ -1,11 +1,19 @@
 package service;
 
+import model.Product;
 import model.ShoppingBasket;
+
+import java.util.List;
 
 public class ShoppingBasketService {
 
 
-    public double totalPrice(ShoppingBasket shoppingBasket){
+    public double totalPrice( ShoppingBasket shoppingBasket ) {
+        List<Product> listOfProducts = shoppingBasket.getListaProduktów();
+
+//        for()
+
+
 
         return 0;
     }
