4b20dc136ff4d1a2fde405a9fbdac3e16653e427
dJaroo <domijaro2005@gmail.com>
2020-03-13 18:12:52 +0100

    as

---
+++ src/service/CreditCardService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.CreditCard;
+
+public class CreditCardService {
+    public boolean checkCardValidtion(CreditCard creditCard)
+    String cvvCode = CreditCard.getCCVCode();
+    String creditCardNumber = CreditCard.getCreditCardNumber();
+    String date = CreditCard.getDate();
+
+    //if(cvvCode.leng != 3 )
+}
