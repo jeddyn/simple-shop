9f703d12965f6750c4b07c09fb58fcfc747d94e6
stasi_bxdcsni <890montis123@gmail.com>
2020-03-13 20:12:55 +0100

    credit card validation v2

--- src/service/CreditCardService.java
+++ src/service/CreditCardService.java
@@ -8,6 +8,8 @@ public class CreditCardService {
         String CVVCode = creditCard.getCVVCode();
         String creditCardNumber = creditCard.getCreditCardNumber();
         String date = creditCard.getDate();
+        String stringDate = date.replace("." ,"");
+
 if(CVVCode.length() != 3) {
     return false;
 }
@@ -16,10 +18,34 @@ if(   CVVCode.charAt(0) < 47 ||CVVCode.charAt(0)  >57||
         CVVCode.charAt(2) < 47 || CVVCode.charAt(2) >57 ) {
 
 }
-if(){
-    return false
+if(creditCardNumber.length() != 19){
+    return false;
 }
+for(int i = 0;i < creditCardNumber.length();i++){
+  if( creditCardNumber.charAt(i) < 47 || creditCardNumber.charAt(i) > 57) {
+      return false;
+  }
+}
+        for(int i = 0;i < stringDate.length();i++){
+            if( stringDate.charAt(i) < 47 || stringDate.charAt(i) > 57) {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
 return true;
 
     }
 }
+
+// 2009.3.12
+// 1950 - 2020.1 -12.1-30
+
+
+
